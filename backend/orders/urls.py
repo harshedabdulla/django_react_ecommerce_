600dc65e1b3386d997bb4e862f355a409a9ed056
Mishal <mishal0404@gmail.com>
2024-04-22 00:25:14 +0530

    fixed commits again

--- backend/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from orders import views
-
-
-urlpatterns = [
-    path('orders/', views.OrderView.as_view(), name="orders-list"),
-    path('order-create/', views.CreateOrderView.as_view(), name="orders-create"),
-]
