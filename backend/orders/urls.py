bb898ed20b01c6af772f4de981e37a9248777bfa
Mishal <mishal0404@gmail.com>
2024-04-21 23:39:38 +0530

    order update

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -5,5 +5,4 @@ from orders import views
 urlpatterns = [
     path('orders/', views.OrderView.as_view(), name="orders-list"),
     path('order-create/', views.CreateOrderView.as_view(), name="orders-create"),
-    path('order/update/<int:pk>/', views.UpdateOrderStatusView.as_view(), name="update-order-status"),
 ]
